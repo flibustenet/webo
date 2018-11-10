507e072a7f4637cde662e38e1dad3eab4c1df399
wilk <wilk@flibuste.net>
2018-11-10 14:23:48 +0100

    middleware en func

--- catcher.go
+++ catcher.go
@@ -32,77 +32,7 @@ type Catcher struct {
 }
 
 func (h *Catcher) ServeHTTP(wrt http.ResponseWriter, r *http.Request) {
-	var logBuf bytes.Buffer
-	lg := log.New(&logBuf, "", log.LstdFlags)
-	lg.Println("------", r.Method, r.URL)
-	fmt.Println(r.Method, r.URL)
-	/* pas glop pour json post
-	if r.Method == "POST" {
-		r.ParseForm()
-		lg.Print("POST = ", r.Form)
-	}
-	*/
-	ctx := context.WithValue(r.Context(), "webo-catcher-log", lg)
-	r = r.WithContext(ctx)
-	w := httptest.NewRecorder()
-	defer func() {
-		if rec := recover(); rec != nil {
-			lg.Println("================== Panic ==============")
-			s := fmt.Sprintf("%+v", rec)
-			if !strings.Contains(s, "runtime.") { // si pas de stack, créée une nouvelle erreur
-				switch x := rec.(type) {
-				case error:
-					lg.Printf("%+v", errors.WithStack(rec.(error)))
-				case string:
-					lg.Printf("%+v", errors.New(x))
-				default:
-					lg.Printf("Unknow error type %s : %v", reflect.TypeOf(rec), rec)
-				}
-			} else {
-				lg.Print(s)
-			}
-			log.Println(logBuf.String())
-			fmt.Println(logBuf.String())
-			if h.debug == 0 && h.url_log != "" {
-				resp, err := http.PostForm(h.url_log, url.Values{"title": {"[bug] " + h.name + "_" + h.version},
-					"version": {h.version},
-					"poste":   {h.poste},
-					"log":     {logBuf.String()}})
-
-				if err != nil {
-					log.Println(err)
-					http.Error(wrt, "500: Un incident s'est produit et n'a pas pu être envoyé à l'administrateur",
-						http.StatusInternalServerError)
-					return
-				}
-				http.Error(wrt, "500: Un incident s'est produit et a été envoyé à l'administrateur",
-					http.StatusInternalServerError)
-				defer resp.Body.Close()
-				return
-			}
-			if h.debug > 0 {
-				fmt.Fprintln(wrt, "<pre>"+logBuf.String()+"</pre>")
-			} else {
-				http.Error(wrt, "500: Un incident s'est produit",
-					http.StatusInternalServerError)
-			}
-		}
-	}()
-
-	h.next.ServeHTTP(w, r)
-	for k, v := range w.Header() {
-		wrt.Header()[k] = v
-	}
-	if wrt.Header().Get("Content-Type") == "" {
-		wrt.Header().Set("Content-Type", "text/html; charset=utf-8")
-	}
-	if w.Code == 0 {
-		w.Code = 200
-	}
-	wrt.WriteHeader(w.Code)
-	wrt.Write(w.Body.Bytes())
-	// ignore broken pipe (client déjà fermé ?)
-	log.Print(logBuf.String())
+	CatcherMiddleware(h.debug, h.name, h.url_log, h.version, h.poste)(h.next).ServeHTTP(wrt, r)
 }
 
 func NewCatcher(debug int, name string, url_log string, version string, poste string, h http.Handler) *Catcher {
@@ -120,4 +50,86 @@ func NewCatcher(debug int, name string, url_log string, version string, poste st
 	log.Printf("Start [%s] version:%s poste:%s\n", name, version, poste)
 	fmt.Printf("Start [%s] version:%s poste:%s\n", name, version, poste)
 	return c
+}
+func CatcherMiddleware(debug int, name string, url_log string, version string, poste string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(wrt http.ResponseWriter, r *http.Request) {
+			var logBuf bytes.Buffer
+			lg := log.New(&logBuf, "", log.LstdFlags)
+			lg.Println("------", r.Method, r.URL)
+			fmt.Println(r.Method, r.URL)
+			/* pas glop pour json post
+			if r.Method == "POST" {
+				r.ParseForm()
+				lg.Print("POST = ", r.Form)
+			}
+			*/
+			ctx := context.WithValue(r.Context(), "webo-catcher-log", lg)
+			r = r.WithContext(ctx)
+			w := httptest.NewRecorder()
+			defer func() {
+				if rec := recover(); rec != nil {
+					if rec_redirect, ok := rec.(ErrRedirect); ok {
+						http.Redirect(wrt, r, rec_redirect.URL, http.StatusSeeOther)
+						return
+					}
+					lg.Println("================== Panic ==============")
+					s := fmt.Sprintf("%+v", rec)
+					if !strings.Contains(s, "runtime.") { // si pas de stack, créée une nouvelle erreur
+						switch x := rec.(type) {
+						case error:
+							lg.Printf("%+v", errors.WithStack(rec.(error)))
+						case string:
+							lg.Printf("%+v", errors.New(x))
+						default:
+							lg.Printf("Unknow error type %s : %v", reflect.TypeOf(rec), rec)
+						}
+					} else {
+						lg.Print(s)
+					}
+					log.Println(logBuf.String())
+					fmt.Println(logBuf.String())
+					if debug == 0 && url_log != "" {
+						resp, err := http.PostForm(url_log, url.Values{"title": {"[bug] " + name + "_" + version},
+							"version": {version},
+							"poste":   {poste},
+							"log":     {logBuf.String()}})
+
+						if err != nil {
+							log.Println(err)
+							http.Error(wrt, "500: Un incident s'est produit et n'a pas pu être envoyé à l'administrateur",
+								http.StatusInternalServerError)
+							return
+						}
+						http.Error(wrt, "500: Un incident s'est produit et a été envoyé à l'administrateur",
+							http.StatusInternalServerError)
+						defer resp.Body.Close()
+						return
+					}
+					if debug > 0 {
+						fmt.Fprintln(wrt, "<pre>"+logBuf.String()+"</pre>")
+					} else {
+						http.Error(wrt, "500: Un incident s'est produit",
+							http.StatusInternalServerError)
+					}
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+			for k, v := range w.Header() {
+				wrt.Header()[k] = v
+			}
+			if wrt.Header().Get("Content-Type") == "" {
+				wrt.Header().Set("Content-Type", "text/html; charset=utf-8")
+			}
+			if w.Code == 0 {
+				w.Code = 200
+			}
+			wrt.WriteHeader(w.Code)
+			wrt.Write(w.Body.Bytes())
+			// ignore broken pipe (client déjà fermé ?)
+			log.Print(logBuf.String())
+
+		})
+	}
 }
