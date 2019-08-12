4f372a4b47af006b2547abeeef33b04836d818a4
wilk <wilk@flibuste.net>
2019-08-12 08:41:40 +0200

    remet tx.go car dépendance avec catcher

---
+++ tx.go
@@ -0,0 +1,49 @@
+package webo
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type TX struct {
+	DB   *sqlx.DB
+	next http.Handler
+}
+
+func RequestTx(r *http.Request) *sqlx.Tx {
+	return r.Context().Value("webo-tx").(*sqlx.Tx)
+}
+
+func (t *TX) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	TxMiddleware(t.DB)(t.next).ServeHTTP(w, r)
+}
+func NewTx(db *sqlx.DB, h http.Handler) *TX {
+	c := &TX{db, h}
+	return c
+}
+
+func TxMiddleware(db *sqlx.DB) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log := RequestCatcherLog(r)
+			tx := db.MustBegin()
+
+			defer func() {
+				if rec := recover(); rec != nil {
+					log.Print("rollback")
+					tx.Rollback()
+					panic(rec)
+				} else {
+					tx.Commit()
+					log.Println("commit")
+				}
+			}()
+
+			ctx := context.WithValue(r.Context(), "webo-tx", tx)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
